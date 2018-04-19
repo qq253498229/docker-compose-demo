ad8346f7b1a55c66a50a286ead068e1340d88429
大胃王 <253498229@qq.com>
2018-04-19 16:20:42 +0800

    加入eureka

---
+++ center-eureka/src/main/java/com/example/CenterEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangbin
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CenterEurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CenterEurekaApplication.class, args);
+  }
+}
